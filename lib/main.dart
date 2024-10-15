44f9dc62f18344bf17ea9da90bbd9e8f8b0a260e
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-15 21:51:16 +0200

    importing packages in dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
+
 void main() {
   runApp();
 }
 
 // the runApp function  is a function (method) wht we don't  write, or built in dart, instead it's provided by the flutter framework.
+// Even though it's defined in the flutter framework, we still need to tell our dart file where to find it..
+// where do we find  it then, it is found int he flutter package which is located in the `pubspec.yaml` file, in the material.dart module
+// we tell dart to get the package by importing it on the top of the file.
